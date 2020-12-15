c934a882eb951a3349edbd2baf63305ac807f336
zm008 <992683374@qq.com>
2020-12-15 12:44:57 +0800

    Initial commit 瑞雯

---
+++ src/main/java/com/atguigu/ruiwen/Test.java
@@ -0,0 +1,9 @@
+package com.atguigu.ruiwen;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("你好");
+        System.out.println("瑞雯");
+    }
+}
